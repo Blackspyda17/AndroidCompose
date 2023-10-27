b7e0ef836507384a2d869f02e292b63017818259
apaez-solutionstream <apaez@solutionstream.com>
2023-10-27 12:22:56 -0600

    Update WaterCounter.kt

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -20,8 +20,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -48,6 +51,17 @@ fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = M
             enabled = count < 10,
             modifier = Modifier.padding(top = 8.dp)
         ) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        val count: MutableState<Int> = remember { mutableIntStateOf(0) }
+        Text("You've had ${count.value} glasses.")
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
